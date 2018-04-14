c8dc36263bb10666058277eeaf58b0517d0f838f
scott quach <scottqglobal@gmail.com>
2018-04-14 09:39:07 -0700

    Canvas course IDs are now listed in the txt file. The user no longer needs to enter the script file

--- canvas_to_todoist.py
+++ canvas_to_todoist.py
@@ -2,35 +2,40 @@ import requests
 import json
 from todoist.api import TodoistAPI
 from requests.auth import HTTPDigestAuth
+from retrieve_canvas_course_ids import load_courses
 
-api_file = open("C:\\Users\\Scott Quach\\Desktop\\Python Stuff\\api_keys.txt")
-keys = api_file.readlines()
-api_file.close()
-print(keys[0])
-print(keys[1])
-
-canvas_api_heading = 'https://canvas.instructure.com'
-canvas_token = keys[0]
+with open("C:\\Users\\Scott Quach\\Desktop\\Python Stuff\\api_keys.txt") as api_file:
+    keys = api_file.readlines()
 
 #Initialize TodoistAPI
-todoist_api_token = keys[1]
+todoist_api_token = keys[0].strip()
 todoist_api = TodoistAPI(todoist_api_token)
 todoist_api.reset_state()
 todoist_api.sync()
 
-#Course ID of the current courses I'm taking
-css_342_id = 100000001130860
-css_301_id = 100000001130859
-css_360_id = 100000001215804
+canvas_api_heading = 'https://canvas.instructure.com'
+canvas_token = keys[1].strip()
 
-course_name_id_dict = {
-                        'CSS 342 Data Structures and Algorithms': css_342_id,
-                        'CSS 301 Technical Writing': css_301_id,
-                        'CSS 360 Software Engineering': css_360_id}
-course_id_name_dict = {
-                        css_342_id: 'CSS 342 Data Structures and Algorithms',
-                        css_301_id: 'CSS 301 Technical Writing',
-                        css_360_id: 'CSS 360 Software Engineering'}
+courses_id_name_dict = load_courses(False)
+# course_name_id_dict = {v:k for k,v in course_id_name_dict.items()}
+
+course_ids = []
+for course_id in keys[2:]:
+    course_ids.append(int(course_id.strip()))
+
+#Course ID of the current courses I'm taking
+# css_342_id = 100000001130860
+# css_301_id = 100000001130859
+# css_360_id = 100000001215804
+
+# course_name_id_dict = {
+#                         'CSS 342 Data Structures and Algorithms': css_342_id,
+#                         'CSS 301 Technical Writing': css_301_id,
+#                         'CSS 360 Software Engineering': css_360_id}
+# course_id_name_dict = {
+#                         css_342_id: 'CSS 342 Data Structures and Algorithms',
+#                         css_301_id: 'CSS 301 Technical Writing',
+#                         css_360_id: 'CSS 360 Software Engineering'}
 
 header = {"Authorization":"Bearer " + canvas_token}
 param = {'per_page': '100', 'include':'submission'}
@@ -42,9 +47,9 @@ todoist_project_dict = {}
 # Iterates over the course_name_id_dict and loads all of the users assignments
 # for those classes. Appends assignment objects to assignments list
 def load_assignments():
-    for course_name in course_name_id_dict:
+    for course_id in course_ids:
         response = requests.get(canvas_api_heading + '/api/v1/courses/' +
-        str(course_name_id_dict[course_name]) + '/assignments', headers=header,
+        str(course_id) + '/assignments', headers=header,
         params=param)
 
         for assignment in response.json():
@@ -66,9 +71,9 @@ def load_todoist_projects():
 # Checks to see if the user has a project matching their course names, if there
 # isn't a new project will be created
 def create_todoist_projects():
-    for course_name in course_name_id_dict:
-        if course_name not in todoist_project_dict:
-            project = todoist_api.projects.add(course_name)
+    for course_id in course_ids:
+        if courses_id_name_dict[course_id] not in todoist_project_dict:
+            project = todoist_api.projects.add(courses_id_name_dict[course_id])
             todoist_api.commit();
             todoist_api.sync()
 
@@ -80,7 +85,7 @@ def create_todoist_projects():
 # to make sure the assignment has not already been trasnfered to prevent overlap
 def transfer_assignments_to_todoist():
     for assignment in assignments:
-        course_name = course_id_name_dict[assignment['course_id']]
+        course_name = courses_id_name_dict[assignment['course_id']]
         assignment_name = assignment['name']
         project_id = todoist_project_dict[course_name]
 
@@ -107,10 +112,10 @@ def add_new_task(assignment, project_id):
     test_task = todoist_api.items.add(assignment['name'] + ' Due', project_id,
     date_string=assignment['due_at'])
 
-# load_todoist_projects()
-# load_assignments()
-# load_todoist_tasks()
-#
-# create_todoist_projects()
-#
-# transfer_assignments_to_todoist()
+load_todoist_projects()
+load_assignments()
+load_todoist_tasks()
+
+create_todoist_projects()
+
+transfer_assignments_to_todoist()
---
+++ retrieve_canvas_course_ids.py
@@ -0,0 +1,24 @@
+import requests
+
+with open("C:\\Users\\Scott Quach\\Desktop\\Python Stuff\\api_keys.txt") as api_file:
+    keys = api_file.readlines()
+
+canvas_api_heading = 'https://canvas.instructure.com'
+canvas_token = keys[1].strip()
+
+header = {'Authorization': 'Bearer ' + canvas_token}
+param = {'per_page': 100}
+
+def load_courses(should_print):
+    response = requests.get(canvas_api_heading + '/api/v1/courses',
+         headers=header, params=param)
+
+    courses_id_name_dict = {}
+
+    for course in response.json():
+        courses_id_name_dict[course.get('id', None)] = course.get('name', None)
+        if should_print:
+            print(course.get('name', "Couldn't load") + ': ' + str(course.get('id', "")))
+    return courses_id_name_dict
+
+load_courses(True)
