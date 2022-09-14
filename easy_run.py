c60844f9598d62f42248637cfa34022f9f5d0298
Jesse <github@safetacks.info>
2022-09-14 08:40:57 -0600

    Cleanup lock logic

--- easy_run.py
+++ easy_run.py
@@ -186,15 +186,14 @@ def transfer_assignments_to_todoist():
                     print("Ignoring ungraded/submittable assignment: " + course_name + ": " + assignment['name'])
                     is_added = True
                     break
-            if assignment['unlock_at'] != None and config['sync_locked_assignments'] == False:
-                if assignment['unlock_at'] > (datetime.datetime.now() + datetime.timedelta(days=1)).isoformat():
-                    print("Ignoring assignment that is locked: " + course_name + ": " + assignment['name'] + ": " + assignment['lock_explanation'])
-                    is_added = True
-                    break
-            # if assignment['locked_for_user'] == True and config['sync_locked_assignments'] == False:
-                # print("Ignoring assignment that is locked: " + course_name + ": " + assignment['name'] + ": " + assignment['lock_explanation'])
-                # is_added = True
-                # break
+            if assignment['unlock_at'] != None and config['sync_locked_assignments'] == False and assignment['unlock_at'] > (datetime.datetime.now() + datetime.timedelta(days=1)).isoformat():
+                print("Ignoring assignment that is not yet unlocked: " + course_name + ": " + assignment['name'] + ": " + assignment['lock_explanation'])
+                is_added = True
+                break
+            if assignment['locked_for_user'] == True and assignment['unlock_at'] == None and config['sync_locked_assignments'] == False:
+                print("Ignoring assignment that is locked: " + course_name + ": " + assignment['name'] + ": " + assignment['lock_explanation'])
+                is_added = True
+                break
             if assignment['due_at'] == None and config['sync_no_due_date_assignments'] == False:
                 print("Ignoring assignment with no due date: " + course_name + ": " + assignment['name'])
                 is_added = True
